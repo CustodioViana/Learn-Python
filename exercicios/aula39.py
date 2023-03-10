7b07120558e6f63fbd562c6eb3794a0f61e53d08
custodioviana <custodioviana@sapo.pt>
2023-03-10 14:04:40 +0000

    new

--- exercicios/aula39.py
+++ exercicios/aula39.py
@@ -1,3 +1,4 @@
+linha = (20*'-')
 """
 Iterando strings com while
 """
@@ -12,13 +13,44 @@ Iterando strings com while
 # nova_string = ''
 # nova_string += '*L*u*i*z* *O*t*á*v*i*o'
 
+# print('Minha resolução')
+# print(linha)
+
+# nome = input('Please, enter your name: ')
+# cont_nome = len(nome)
+# letra = 0
+
+# while letra <= (cont_nome - 1):
+#     print('*', nome[letra], '*')
+#     letra += 1
+
+# print(f'Seu nome tem {cont_nome} letras')
+
+# print(linha)
+# print('Resolução do professor')
+
+# nome = 'Maria Helena'  # Iteráveis
+
+# indice = 0
+# novo_nome = ''
+# while indice < len(nome):
+#     letra = nome[indice]
+#     novo_nome += f'*{letra}'
+#     indice += 1
+
+# novo_nome += '*'
+# print(novo_nome)
+
+
+print(linha)
+print('Reestruturação do execercício')
 
 nome = input('Please, enter your name: ')
-cont_nome = len(nome)
-letra = 0
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
 
-while letra <= (cont_nome - 1):
-    print(nome[letra])
-    letra += 1
-
-print(f'Seu nome tem {cont_nome} letras')
+print(f'{novo_nome}*')
