0108ba5f4f21ba15ebcee093d9c39743f79342a1
custodioviana <custodioviana@sapo.pt>
2023-03-10 10:14:38 +0000

    new

---
+++ iniciante/aula34.py
@@ -0,0 +1,26 @@
+print('Estruturas de repetição')
+
+"""
+while (enquanto)
+Esecuta uma ação enquanto for verdadeiro
+"""
+
+# condicao = True
+
+# while condicao:
+#     nome = input('Qual o seu nome: ')
+#     print(f'Seu nome é {nome}')
+
+#     if nome == 'sair':
+#         break
+
+# print('Acabou')
+
+
+contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
