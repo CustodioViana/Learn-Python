01cb71c9faae28cfac31bab0bf9fbd32edece5ff
custodioviana <custodioviana@sapo.pt>
2023-03-10 10:50:29 +0000

    new

--- iniciante/aula34.py
+++ iniciante/aula34.py
@@ -1,9 +1,13 @@
 print('Estruturas de repetição')
 
+linha = 20*'-'
 """
 while (enquanto)
 Esecuta uma ação enquanto for verdadeiro
+break -> usaod para para loop
 """
+
+print('while e break')
 
 # condicao = True
 
@@ -16,11 +20,88 @@ Esecuta uma ação enquanto for verdadeiro
 
 # print('Acabou')
 
+print(linha)
 
-contador = 0
+print('Condições aritiméticas')
+# contador = 0
 
-while contador <= 10:
-    contador = contador + 1
-    print(contador)
+# while contador <= 10:
+#     contador = contador + 1
+#     print(contador)
 
-print('Acabou')
+# print('Acabou')
+
+print(linha)
+
+print('Condições aritiméticas + operadores aritiméricos')
+
+
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+# contador = 0
+
+# contador += 5
+# print(contador)
+
+# linha
+
+# contador = 0
+# while contador <=10:
+#     print(contador)
+#     contador += 5
+
+print(linha)    
+
+print('while e continue')
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+# contador = 0
+
+# while contador <= 100:
+#     contador += 1
+
+#     if contador == 6:
+#         print('Não vou mostrar o 6.')
+#         continue
+
+#     if contador >= 10 and contador <= 27:
+#         print('Não vou mostrar o', contador)
+#         continue
+
+#     print(contador)
+
+#     if contador == 40:
+#         break
+
+
+# print('Acabou')
+
+print(linha)
+
+print('while dentro de while')
+
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+# qtd_linhas = 5
+# qtd_colunas = 5
+
+# linha = 1
+# while linha <= qtd_linhas:
+#     coluna = 1
+#     while coluna <= qtd_colunas:
+#         print(f'{linha=} {coluna=}')
+#         coluna += 1
+#     linha += 1
+
+
+# print('Acabou')
