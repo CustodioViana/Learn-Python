239eeea65d47284b8df56a3ff889332e097d7743
custodioviana <custodioviana@sapo.pt>
2023-03-14 16:02:37 +0000

    midfi

--- exercicios/47_excercicio_for.py
+++ exercicios/47_excercicio_for.py
@@ -8,37 +8,70 @@ Faça a contagem de tentativas do seu usuário.
 """
 
 
+
+palavra_secreta = 'perfume'
+letras_acertadas = ''
+numero_tentativas = 0
+
+while True:
+    letra_digitada = input('Digite uma letra: ')
+    numero_tentativas += 1
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra.')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'
+
+    print('Palavra formada:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        print('VOCÊ GANHOU!! PARABÉNS!')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas:', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
+
+
+
+
+
+
 # Primeira Tentativa
 
-p_secreta = input('Digite uma palavra: ')
-l_achadas = []
-tentativa = 0
+# palavra_secreta = input('Digite uma palavra: ')
+# letras_achadas = []
+# tentativas = 0
 
+# for i in palavra_secreta:
+#     letras_achadas.append('*')
 
-for i in range(len(p_secreta)):
-    l_achadas.append('*')
-
-acertou = False
-
-while acertou == False:
-    tentativa += 1
+# while True:
+#     letras = input('Digite uma letra: ')
+#     if len(letras) > 1:
+#         print('Digite somente uma letra')
+#     elif len(letras) == 0:
+#         print('Digite pelo menos uma letra')
+#         continue
     
-    letra = input('Digite uma letra: ')
-    for i in range(len(p_secreta)):
-        if letra == p_secreta[i]:
-            l_achadas[i] = letra
-            print(l_achadas)
+#     tentativas += 1
 
+#     if letras in palavra_secreta:
+#         letras_achadas += letras
 
-acertou = True
-
-for j in range(len(l_achadas)):
-    if l_achadas == '*':
-        acertou = False
-
-
-
-
+#     for i in range(len(palavra_secreta)):
+#         if i in letras_achadas:
+#             print(i)
+#         else:
+#             print('*')
 
 
     
--- teste/2_for.py
+++ teste/2_for.py
@@ -1,5 +1,11 @@
 palavra_secreta = input(': ')
+letra_ok = ''
 
+while True:
+    letra = input('Digite uma letra: ')
+    for i in range(len(palavra_secreta)):
+        if letra == palavra_secreta[i]:
+            letra_ok = letra
 
-for i in range(len(palavra_secreta)):
-    print(palavra_secreta[i])
+    print(letra_ok)
+
