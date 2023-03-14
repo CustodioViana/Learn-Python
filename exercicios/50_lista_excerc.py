763d78367bcc28db7bc226f08a3e0de3095e0ca7
custodioviana <custodioviana@sapo.pt>
2023-03-14 21:07:22 +0000

    news

---
+++ exercicios/50_lista_excerc.py
@@ -0,0 +1,14 @@
+lista = ['Maria', 'Helena', 'luiz']
+lista.append('Marcos')
+lista.append('Alberto')
+lista.pop()
+lista.append('Deise')
+lista[0] = 'Marta'
+
+i = range(len(lista))
+
+novo_nome = i
+
+for indice in i:
+    print(indice, lista[indice])
+
---
+++ teste/3_listas.py
@@ -0,0 +1,13 @@
+
+# Testes com extender
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = [7, 8, 9]
+# lista_a.extend(lista_b + lista_c)
+lista_a.extend(lista_b)
+lista_a.extend(lista_c)
+
+# lista_a.extend(lista_c)
+
+print(lista_a)
